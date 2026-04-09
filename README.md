18d30abd1951be187011111305ac6edf928dd2d3
unknown <giogoncalvespadua13@gmail.com>
2026-04-09 09:12:30 -0300

    Organização do trabalho

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Projeto Avaliativo - Sistema Web de Gerenciamento de Atividades e Avaliações para Escolas
+
+## Nome e descrição 
+
+## Tecnologias usadas
+
+## Pré-requisitos
+
+## Como instalar
+
+## Scripts disponíveis
+
+## Estrutura de pastas 
+
+## Como contribuir
+
+## Licença e autores
---
+++ assets/scripts/modules/Avaliacao.js
@@ -0,0 +1,18 @@
+class Avaliacao {
+    // Cadastrar avaliação
+    constructor (id, titulo, disciplina, turma, data, numQuestoes, status, valor)
+    {
+        this.id = id
+        this.titulo = titulo
+        this.disciplina = disciplina
+        this.turma = turma
+        this.dataAplicacao = data
+        this.numeroQuestoes = numQuestoes
+        this.status = status
+        this.valorAvalicao = valor
+    }
+
+    //
+
+    
+}
